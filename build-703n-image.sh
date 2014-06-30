6f0e85d62e37baf7cbcf67819eb72ae49f0a8c54
Ubuntu <ubuntu@ubuntu.(none)>
2014-06-30 17:20:03 -0400

    Allow build-703n-image to be run from anywhere

--- build-703n-image.sh
+++ build-703n-image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # OpenWrt Image Builder image should be located in ~/openwrt-imagebuilder
-# Also expects ~/cross-compile-bfgminer.sh
+# Also expects ./cross-compile-bfgminer.sh
 
 # set these
 bfgminer_version="4.2.99"
@@ -10,8 +10,9 @@ image_version="pr2"
 # not these
 full_version="v${bfgminer_version}-${image_version}"
 image_output_dir="${HOME}/openwrt-imagebuilder/bin/ar71xx"
+base_dir=$(dirname $0)
 
-sudo "${HOME}/cross-compile-bfgminer.sh"
+sudo "${base_dir}/cross-compile-bfgminer.sh"
 cd "${HOME}/openwrt-imagebuilder"
 rm -rf tmp && make image PROFILE=TLWR703 PACKAGES="libcurl libpthread jansson libusb-1.0 kmod-usb-serial kmod-usb-serial-cp210x kmod-usb-serial-ftdi kmod-usb-acm -kmod-gpio-button-hotplug hotplug2 -dnsmasq -uboot-envtools -6relayd -libgcc -libc -firewall -iptables -ip6tables -swconfig -ppp -ppp-mod-pppoe -kmod-ipt-nathelper -kmod-ledtrig-netdev libncurses screen" FILES=files/
 
